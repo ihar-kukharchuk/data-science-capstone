cc51e46f205ece33d093eb24f07118899e1da78b
Ihar Kukharchuk <ihar.kukharchuk@gmail.com>
2019-09-12 05:12:56 +0000

    Integrate checkpoint, pacman and config packages

---
+++ nlp/run_analyses.R
@@ -0,0 +1,33 @@
+# rm(list = ls(all.names = TRUE)) # clean up workspace
+
+CheckPackage <- function(packages) {
+    packages.to.install <- setdiff(packages, rownames(installed.packages()))
+    if (length(packages.to.install) > 0) {
+        install.packages(packages.to.install)
+    }
+}
+
+SetupDevEnvironment <- function() {
+    CheckPackage("checkpoint")
+    # checkpoint package asks for permission to create that folder if it doesn't
+    # exist; let's allow to do so without asking
+    # https://github.com/RevolutionAnalytics/checkpoint/blob/master/R/checkpoint_paths.R +62
+    checkpoint.path <- "~/.checkpoint"
+    dir.create(checkpoint.path, recursive = TRUE)
+    if (!dir.exists(checkpoint.path)) {
+        stop(paste0("unable to create directory with path: ", checkpoint.path))
+    }
+    checkpoint::checkpoint("2019-09-11", scanForPackages = FALSE)
+    # install checkpoint one more time in checkpoint-ed environment in order to
+    # avoid warnings from IDE
+    CheckPackage(c("checkpoint", "pacman"))
+}
+SetupDevEnvironment()
+
+pacman::p_load("config")
+
+Main <- function() {
+    config::get("corpus")
+}
+
+Main()
